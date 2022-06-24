6d542766bb4b980024397a0c04a827056eda3c7f
Cris IELUSC <cristofer.sousa@ielusc.br>
2022-06-23 21:37:36 -0300

    fix: remove credentials for connect supabase

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL_API = 'https://ozuygrtocdubmsatevyd.supabase.co'
-const TOKEN_API = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dXlncnRvY2R1Ym1zYXRldnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM2MDkyNDEsImV4cCI6MTk2OTE4NTI0MX0.AsJwHChGz3Z67-gNl5_KMoNV9NaArGTx01uOAfd8ZXo';
+const URL_API = process.env.SUPABASE_URL;
+const TOKEN_API = process.env.SUPABASE_TOKEN;
 
 const PERSONS_API = createClient(URL_API, TOKEN_API)
 
